7c0e8041779db28c1478f037d8152677a9c70a59
Robert Rachwal <robertr16@onet.eu>
2018-03-11 15:23:31 +0000

    Funkcja fibo.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <stdio.h>
+#include <iostream>
+
+int fibo(const int n) {
+        int l1 = 0;
+        int l2 = 1;
+        int l;
+        for (int i = 0; i < n-1; i++) {
+                l = l1 + l2;
+                l1 = l2;
+                l2 = l;
+        }
+        return l;
+}
 
 int main()
 {
-	printf("hello, world!\n");
-	return 0;
-}
+        std::cout << "hello, world!\n";
+        int i;
+        std::cin >> i;
+        int f_i;
+        f_i = fibo(i);
+        std::cout << i <<"-ty wyraz ciagu Fibonacciego to: " << f_i <<'\n';
 
+    return 0;
+}
