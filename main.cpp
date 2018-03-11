8f44f392860e05497289da4a71206fcb322119c0
Lukasz Zurek <zurozuro@gmail.com>
2018-03-11 15:24:19 +0000

    Last minor fixes.

--- main.cpp
+++ main.cpp
@@ -3,16 +3,13 @@
 int fibo(int n)
 {
 	if (n > 2) {
-		int wynik = fibo(n-1) + fibo(n-2);
-		return wynik;
+		return fibo(n-1) + fibo(n-2);
 	}
 	return 1;
 }
-
 
 int main()
 {
 	std::cout << "hello, world!" << std:endl;
 	return 0;
 }
-
