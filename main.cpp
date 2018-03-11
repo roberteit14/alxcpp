c5df4f66fcae85727de7ef5e670a82815d3510b4
Lukasz Zurek <zurozuro@gmail.com>
2018-03-11 14:47:46 +0000

    Added magic Fibo function.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+int fibo(int n)
+{
+	if (n > 2){
+
+		int wynik = fibo(n-1) + fibo(n-2);
+		std::cout << n << "\n";
+		return wynik;
+	}
+	return 1;
+}
+
+
 int main()
 {
 	std::cout << "hello, world!" << std:endl;
